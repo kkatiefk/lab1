fbed17023be7c195ee14c4bda1ce96225839554b
Katerina <161355341+kkatiefk@users.noreply.github.com>
2024-03-05 19:53:15 +0200

    Add files via upload

---
+++ 2sem lab1/Program.cs
@@ -0,0 +1,21 @@
+﻿List<string> listString = new List<string>()
+{
+    "Попрощалось ясне сонце",
+    "З чорною землею.",
+    "Виступає круглий мiсяць",
+    "З сестрою зорею...",
+    "I темний гайок зелененький,",
+    "I мiсяць з зорями сiяв,",
+    "I соловейко на калинi",
+    "То затихав, то щебетав..."
+};
+listString.Sort((x, y) => x.Length.CompareTo(y.Length));
+
+Console.WriteLine("Рядки по зростанню їх довжини:");
+Console.WriteLine();
+foreach (string str in listString)
+{
+    Console.WriteLine(str);
+}
+
+Console.ReadKey();
